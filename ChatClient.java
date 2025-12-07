98d3ea48d73c803bb9918587673a0ed30d73bfd9
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2025-12-08 00:33:56 +0600

    Update the Client to send a message

--- ChatClient.java
+++ ChatClient.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class ChatClient {
     public static void main(String[] args) {
@@ -9,7 +10,17 @@ public class ChatClient {
             Socket socket = new Socket("localhost", 5000);
             System.out.println("Connected to server!");
 
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter message: ");
+            String message = scanner.nextLine();
+
+            out.println(message);
+
+            out.close();
             socket.close();
+            scanner.close();
 
         } catch (IOException e) {
             e.printStackTrace();
